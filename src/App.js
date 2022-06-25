6c476330f906c72d2d7f82e3d57529f317239e47
Nicholas Lona <101734110+nicklona17@users.noreply.github.com>
2022-06-25 12:22:08 -0400

    changed hash router back to browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/home/Home';
 import CreatePost from './components/createPost/CreatePost';
 import Login from './components/login/Login';
